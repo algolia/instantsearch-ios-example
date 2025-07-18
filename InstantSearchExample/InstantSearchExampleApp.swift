953cb44ea37fd4fe44f33893e63bab3f9a835324
Mouaad Aallam <mouaad.aallam@algolia.com>
2025-07-18 12:13:19 +0200

    Initial Commit

---
+++ InstantSearchExample/InstantSearchExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantSearchExampleApp.swift
+//  InstantSearchExample
+//
+//  Created by Mouaad Aallam on 18/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstantSearchExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
